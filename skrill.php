f1d7fca060eb21ca42986436383c60de8bb4852b
ASHADUR ROB <robengg@live.com>
2014-03-09 00:01:10 +0600

    Create skrill.php

---
+++ skrill.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * CodeIgniter Moneybookers (Skrill) libraies
+ *
+ *  PHP 5.1.6 or newer
+ *  author : ASHADUR ROB 
+ *  author_url : http://www.robengg.com
+ *  packages : Codeigniter
+ *  license : GLP
+ *  version : 1.0
+ */
+
+
+//skrill class
+class Skrill {
+    //new array
+    var $config = Array();
+    //skrill url
+    var $production_url = 'https://www.moneybookers.com/app/payment.pl?';
+
+    // function construct 
+    public function __construct($value = array()) {
+        $this->__initialize($value);
+
+    }
+
+    //function init
+    function __initialize($value = array()) {
+        $config ['pay_to_email'] = 'probengalcom@gmail.com';
+        $config ['status_url'] = 'probengalcom@gmail.com';
+        $config ['language'] = 'EN';
+        $config ['return_url_text'] = 'Return to probengal.com';
+        $config ['logo_url'] = 'http://www.moneybookers.com/images/logos/checkout_logos/checkout_240x80px.gif';
+        $config ['return_url'] = '';
+        $config ['cancel_url'] = '';
+        $config ['detail1_description'] = 'Membership';
+        
+        //$config array to class variable
+        if (count($props) > 0) {
+            foreach ($props as $key => $val) {
+                $config[$key] = $val;
+            }
+        }
+        $this->config = $config;
+    }
+    //function pay 
+    function pay() {
+        $vars = http_build_query($this->config);
+        //header function built url
+           header('LOCATION:' . $this->production_url . $vars);    
+    }
+
+}
