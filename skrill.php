069764a0e801a2025ffadcb2b44082224fd20bc9
ROB ASHADUR <robengg@live.com>
2017-06-11 22:41:05 +0900

    Some Value Changes

--- skrill.php
+++ skrill.php
@@ -30,14 +30,14 @@ class Skrill {
 
     //function init
     function __initialize($value = array()) {
-        $config ['pay_to_email'] = 'probengalcom@gmail.com';
-        $config ['status_url'] = 'probengalcom@gmail.com';
+        $config ['pay_to_email'] = 'email@mail.com';
+        $config ['status_url'] = 'email@mail.com';
         $config ['language'] = 'EN';
-        $config ['return_url_text'] = 'Return to probengal.com';
+        $config ['return_url_text'] = 'Return to Domain';
         $config ['logo_url'] = 'http://www.moneybookers.com/images/logos/checkout_logos/checkout_240x80px.gif';
         $config ['return_url'] = '';
         $config ['cancel_url'] = '';
-        $config ['detail1_description'] = 'Membership';
+        $config ['detail1_description'] = 'Some Paymant Description';
         
         //$config array to class variable
         if (count($props) > 0) {
